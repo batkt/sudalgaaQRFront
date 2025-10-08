fad52b8cc68fe976d260ea941424647f490c79ed
bilgeedev <jrrsenpai@gmail.com>
2025-10-08 17:33:59 +0800

    zagwar zaslaa

--- app/page.js
+++ app/page.js
@@ -21,21 +21,19 @@ function Nevtrekh() {
 
   return (
     <div className="flex justify-center items-center bg-[url('/assets/bg-from-unsplash.jpg')] bg-cover bg-center bg-no-repeat w-screen h-screen dark:bg-gray-800 relative">
+      <div className="absolute inset-0 bg-black opacity-40"></div>
       <Head>
         <title>Нэвтрэх хуудас</title>
         <link rel="icon" href="/assets/logo.webp" />
       </Head>
-      <div className="w-full sm:w-[640px] md:w-[768px] lg:w-[1024px] h-fit rounded-xl bg-blue-300 lg:flex hidden justify-center items-center">
+      <div className="w-full sm:w-[640px] md:w-[768px] lg:w-[1024px] h-fit rounded-3xl bg-blue-800 lg:flex hidden justify-center items-center relative z-10">
         <div className="flex flex-col justify-end items-center w-[40%] !h-[408px] rounded-l-xl bg-[url('/assets/subBg.png')] bg-contain bg-center bg-no-repeat py-10 px-20"></div>
-        <div className="block xl:flex justify-start items-center w-[60%] bg-white p-20 pt-10 rounded-r-xl">
+        <div className="block xl:flex justify-start items-center w-[60%] bg-white p-20 rounded-r-3xl">
           <div className="flex w-full h-screen xl:h-auto">
             <div className="my-auto border-r- bg-white dark:bg-gray-900 xl:dark:bg-gray-800 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none !w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
               <div className="flex flex-col items-center justify-start w-full mb-10">
-                <div className="flex items-center justify-center w-20 h-20 overflow-hidden">
-                  <img
-                    className="w-full h-full object-cover scale-[1.5]"
-                    src="/assets/logo.webp"
-                  />
+                <div className="flex items-center justify-center overflow-hidden">
+                  <img className="w-20 h-24 bottom-4" src="/assets/logo.webp" />
                 </div>
                 <h2 className="uppercase font-[750] ">
                   ШҮҮХИЙН ШИЙДВЭР ГҮЙЦЭТГЭХ ЕРӨНХИЙ ГАЗАР
